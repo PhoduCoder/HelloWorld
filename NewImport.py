55c786af747ddbd7a32fcb7227af94f726859276
PhoduCoder <gauravprashar17@gmail.com>
2017-07-26 09:44:34 +0530

    Update NewImport.py

--- NewImport.py
+++ NewImport.py
@@ -1,1 +1,23 @@
-import 
+#!/usr/bin/python
+
+import ExampleImport
+import imp
+
+print ("This is from the python file and not the module")
+
+import ExampleImport
+# NOTE that this import of the module will have
+# no effect
+
+
+# To reload a module in the same program
+# we use the imp module reload method
+
+imp.reload(ExampleImport)
+
+# By virtue of this import
+# this will again execute the print statement 
+# that was a part of the ExampleImport Module
+
+
+
